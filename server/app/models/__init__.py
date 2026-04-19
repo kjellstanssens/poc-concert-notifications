c6281ef8cd4ea64854e05d32be8d3a56634f6c6b
kjellstanssens <kjell.stanssens@student.hogent.be>
2026-04-19 18:22:29 +0200

    refactor: update model and schema exports

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -2,12 +2,18 @@ from .province import Province
 from .venue import Venue
 from .performer import Performer
 from .concert import Concert
+from .user import User
+from .subscription import Subscription
+from .notification import NotificationQueue
 from .assocations import concert_performers
 
 __all__ = [
     "Province",
     "Venue",
     "Performer",
-    "Concert"
+    "Concert",
+    "User",
+    "Subscription",
+    "NotificationQueue",
     "concert_performers",
 ]
--- server/app/schemas/__init__.py
+++ server/app/schemas/__init__.py
@@ -1,4 +1,4 @@
-from .venue import Venue, VenueCreate, VenueBase
+from .venue import Venue, VenueCreate, VenueUpdate, VenueBase
 from .performer import Performer, PerformerCreate, PerformerBase
 from .concert import Concert, ConcertCreate, ConcertBase
 from .user import User, UserCreate, UserBase
